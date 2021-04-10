87776325a3d5bc32f6af0ec3c2f036bcd96c2a34
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2021-04-10 18:16:27 +0200

    changed message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bamse og Kylling (og ælling). Edit <code>src/App.js</code> and save to reload.
+          Bamse og Kylling
         </p>
         <a
           className="App-link"
